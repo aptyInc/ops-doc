795bc4a4bda8ab0712fde38b7644ace7332f7495
Sam <sam.sundar@apty.io>
2021-08-11 15:32:57 +0530

    Fix Sumologic Values.yaml with latest version.

--- 04-K8s/02-Sumologic/setup.sh
+++ 04-K8s/02-Sumologic/setup.sh
@@ -4,7 +4,7 @@ set -e
 source .env
 sed -i -e "s|ACCESS_ID_ENV|$accessId|g" -e "s|ACCESS_KEY_ENV|$accessKey|g" -e "s|CLUSTER_NAME_ENV|$clusterName|g" -e "s|NAMESPACE_ENV|$namespace|g" ./setup.sh
 helm repo add sumologic https://sumologic.github.io/sumologic-kubernetes-collection
-helm upgrade --install sumologic sumologic/sumologic -f values.yaml \
+helm upgrade --install sumologic sumologic/sumologic -f values_v2.1.5.yaml \
   --namespace=NAMESPACE_ENV \
   --create-namespace \
   --set sumologic.accessId="ACCESS_ID_ENV" \
